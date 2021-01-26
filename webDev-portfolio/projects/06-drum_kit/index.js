e0057c325b713911beba82d521f78c075a6d4543
Isaac Villajuana <35943217+ivillajuana@users.noreply.github.com>
2021-01-26 01:11:02 -0600

    Delete webDev-portfolio directory

--- webDev-portfolio/projects/06-drum_kit/index.js
+++
@@ -1,4 +0,0 @@
-
-const drumButtons = document.querySelectorAll(".drum").length;
-
-for (var i=0; i < drumButtons; i++) { document.querySelectorAll(".drum")[i].addEventListener("click", function() {alert("Button clicked") } )  }
